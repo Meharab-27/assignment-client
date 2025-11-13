7a2b74289f10c55e02a25dedabf5af677b7b8ab2
Meharab-27 <meharabrahman100115@gmail.com>
2025-11-13 15:28:13 +0600

    add routes

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -7,12 +7,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBs8MChkn-6ezeAm-KMFJ4M0NX0py4A6x4",
-  authDomain: "assignment-client-e495c.firebaseapp.com",
-  projectId: "assignment-client-e495c",
-  storageBucket: "assignment-client-e495c.appspot.com",
-  messagingSenderId: "163149155489",
-  appId: "1:163149155489:web:f6559f78ae82f8155134e0",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
